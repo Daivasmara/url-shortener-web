eb0988752adffb70a94043234706606a2a73eb5a
Daivasmara <daivasmara@gmail.com>
2020-07-20 16:27:00 +0700

    [FEATURE] MVP Shorten Link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Joi from '@hapi/joi';
 import 'bulma/css/bulma.min.css';
 
+const {
+  REACT_APP_API_ENDPOINT,
+  REACT_APP_REDIRECTOR_DOMAIN,
+  REACT_APP_API_HOST,
+  REACT_APP_API_KEY,
+} = process.env;
+
 function App() {
+  const [link, setLink] = useState('');
+  const [shortenedHash, setShortenedHash] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    setErrorMessage('');
+  }, [link]);
+
+  useEffect(() => {
+    setErrorMessage('');
+    setLink('');
+  }, [shortenedHash]);
+
+  const schema = Joi.object({
+    link: Joi.string().uri().required(),
+  });
+
+  const handlePostReq = async (reqBody) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${REACT_APP_API_ENDPOINT}/v1/link`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Host: REACT_APP_API_HOST,
+          apikey: REACT_APP_API_KEY,
+        },
+        body: JSON.stringify(reqBody),
+      });
+
+      const { message } = await response.json();
+
+      if (response.status !== 201) {
+        throw new Error(message);
+      }
+
+      setIsLoading(false);
+      setShortenedHash(message.hash);
+    } catch (err) {
+      setIsLoading(false);
+      setErrorMessage(err.message);
+    }
+  };
+
+  const handleShorten = async () => {
+    try {
+      const reqBody = await schema.validateAsync({ link });
+      handlePostReq(reqBody);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  const handleKeyDown = (keyCode) => {
+    if (!isLoading && keyCode === 13) {
+      handleShorten();
+    }
+  };
+
+  const shortenedLink = `${REACT_APP_REDIRECTOR_DOMAIN}/${shortenedHash}`;
   return (
     <section className="hero is-warning is-fullheight is-bold">
       <div className="hero-body">
@@ -10,36 +79,51 @@ function App() {
           <div className="columns is-gapless field has-addons ">
             <div className="column is-two-fifths control">
               <input
-                className="input"
+                className={`input ${(errorMessage && 'is-danger') || ''}`}
                 type="text"
                 placeholder="https://put-the-link-here.com"
+                value={link}
+                disabled={isLoading}
+                onChange={({ target: { value } }) => setLink(value)}
+                onKeyDown={({ keyCode }) => handleKeyDown(keyCode)}
               />
               <p
                 className="help is-danger"
                 style={{ position: 'absolute' }}
-              />
-              <p
-                className="help is-success"
-                style={{ position: 'absolute' }}
-              />
+              >
+                {errorMessage}
+              </p>
             </div>
             <div className="column control">
               <button
-                className="button is-warning is-rounded is-inverted"
+                className={`button is-warning is-rounded is-inverted ${(isLoading && 'is-loading') || ''}`}
                 type="button"
+                onClick={() => handleShorten()}
               >
                 Shorten!
               </button>
             </div>
           </div>
-          <div className="modal">
+          <div className={`modal ${(shortenedHash && 'is-active') || ''}`}>
             <div className="modal-background" />
             <div className="modal-content">
               <div className="notification is-warning has-text-centered">
-                <a className="is-size-1 has-text-weight-bold" href="htts://x.sh/ywks">https://x.sh/ywks</a>
+                <a
+                  className="is-size-1 has-text-weight-bold"
+                  href={shortenedLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {shortenedLink}
+                </a>
               </div>
             </div>
-            <button className="modal-close is-large" type="button" aria-label="close" />
+            <button
+              className="modal-close is-large"
+              type="button"
+              aria-label="close"
+              onClick={() => setShortenedHash('')}
+            />
           </div>
         </div>
       </div>
